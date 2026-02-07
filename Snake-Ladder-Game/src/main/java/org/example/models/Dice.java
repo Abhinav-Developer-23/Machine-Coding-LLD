6ccfe31a48abc3aecd504f8ae745393ad3ce4866
Abhinav <abhinav23anand@gmail.com>
2026-02-07 22:47:38 +0530

    commit

--- Snake-Ladder-Game/src/main/java/org/example/models/Dice.java
+++ Snake-Ladder-Game/src/main/java/org/example/models/Dice.java
@@ -2,17 +2,21 @@ package org.example.models;
 
 import lombok.Data;
 
+import java.util.Random;
+
 @Data
 public class Dice {
   private final int minValue;
   private final int maxValue;
+  private final Random random;
 
   public Dice(int minValue, int maxValue) {
     this.minValue = minValue;
     this.maxValue = maxValue;
+    this.random = new Random();
   }
 
   public int roll() {
-    return (int) (Math.random() * (maxValue - minValue + 1) + minValue);
+    return random.nextInt(minValue, maxValue + 1);
   }
 }
